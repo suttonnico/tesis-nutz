04b4317b591ec28dd734dd3cbde75de63f79c3e0
suttonnico <suttonnico2@gmail.com>
2019-05-18 17:27:48 -0300

    a

--- build_dataset.py
+++ build_dataset.py
@@ -47,10 +47,10 @@ s4, img_4 = camera_4.read()
 s6, img_6 = camera_6.read()
 time.sleep(0.5)
 
-s0, img_0 = camera_0.read()
-s2, img_2 = camera_2.read()
+#s0, img_0 = camera_0.read()
+#s2, img_2 = camera_2.read()
 s4, img_4 = camera_4.read()
-s6, img_6 = camera_6.read()
+#s6, img_6 = camera_6.read()
 time.sleep(0.5)
 
 #s6, img_6 = camera_6.read()
@@ -63,10 +63,10 @@ time.sleep(0.5)
 #s6, img_6 = camera_6.read()
 
 
-#cv2.imwrite('data/test2_0.jpg', img_0)
-#cv2.imwrite('data/test2_2.jpg', img_2)
+cv2.imwrite('data/test2_0.jpg', img_0)
+cv2.imwrite('data/test2_2.jpg', img_2)
 cv2.imwrite('data/test2_4.jpg', img_4)
-#cv2.imwrite('data/test2_6.jpg', img_6)
+cv2.imwrite('data/test2_6.jpg', img_6)
 
 exit()
 device_paths = glob.glob('/dev/video*')
