3bb3ed49bb1ac28f413be3ae9eff61e88309938d
suttonnico <suttonnico2@gmail.com>
2019-05-18 17:34:50 -0300

    game

--- game_test.py
+++ game_test.py
@@ -2,7 +2,6 @@ import pygame
 import pygame.camera
 
 pygame.camera.init()
-pygame.camera.list_camera() #Camera detected or not
 cam0 = pygame.camera.Camera("/dev/video0",(640,480))
 cam0.start()
 img = cam0.get_image()
