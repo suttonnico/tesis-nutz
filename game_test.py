514b339c036043a6b4fb82d528e4e6de2078159a
suttonnico <suttonnico2@gmail.com>
2019-05-18 18:17:26 -0300

    game

--- game_test.py
+++ game_test.py
@@ -1,11 +1,11 @@
 import pygame
-import pygame.camera
+import pygame.camera as cam
 import time
-pygame.camera.init()
-cam0 = pygame.camera.Camera("/dev/video0",(640,480))
-cam2 = pygame.camera.Camera("/dev/video2",(640,480))
-cam4 = pygame.camera.Camera("/dev/video4",(640,480))
-cam6 = pygame.camera.Camera("/dev/video6",(640,480))
+cam.init()
+cam0 = cam.Camera("/dev/video0",(640,480))
+cam2 = cam.Camera("/dev/video2",(640,480))
+cam4 = cam.Camera("/dev/video4",(640,480))
+cam6 = cam.Camera("/dev/video6",(640,480))
 
 
 cam0.start()
@@ -13,23 +13,8 @@ cam2.start()
 cam4.start()
 cam6.start()
 start = time.time()
+time.sleep(2)
 down_time = 1
-img0 = cam0.get_image()
-time.sleep(down_time)
-img2 = cam2.get_image()
-time.sleep(down_time)
-img4 = cam4.get_image()
-time.sleep(down_time)
-img6 = cam6.get_image()
-time.sleep(down_time)
-img0 = cam0.get_image()
-time.sleep(down_time)
-img2 = cam2.get_image()
-time.sleep(down_time)
-img4 = cam4.get_image()
-time.sleep(down_time)
-img6 = cam6.get_image()
-time.sleep(down_time)
 img0 = cam0.get_image()
 time.sleep(down_time)
 img2 = cam2.get_image()
