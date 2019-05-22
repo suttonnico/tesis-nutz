e3a462547d3d1ad45cd05e5b71104a7ce1302e10
suttonnico <suttonnico2@gmail.com>
2019-05-22 18:12:15 -0300

    game

--- i2c_module.py
+++ i2c_module.py
@@ -11,20 +11,29 @@ bus = SMBus(1) # indicates /dev/ic2-1
 
  # switch it on
 def go():
-        bus.write_byte(addr,ord('3'))
+		bus.write_byte(addr,ord('3'))
 
 def stop():
-        bus.write_byte(addr,ord('1'))
+		bus.write_byte(addr,ord('1'))
 
 def openA1():
-        bus.write_byte(addr,ord('U'))
+		bus.write_byte(addr,ord('U'))
 
 def closeA1():
-        bus.write_byte(addr,ord('u'))
+		bus.write_byte(addr,ord('u'))
 
 def openA2():
-        bus.write_byte(addr,ord('V'))
+		bus.write_byte(addr,ord('V'))
 
 def closeA2():
-        bus.write_byte(addr,ord('v'))
+		bus.write_byte(addr,ord('v'))
 
+def closeB1():
+		bus.write_byte(addr,ord('x'))
+
+
+def openB1():
+	bus.write_byte(addr, ord('X'))
+
+def closeB2():
+	bus.write_byte(addr, ord('y'))
