71de990056b5267bab14fe842b4edc574588b548
suttonnico <suttonnico2@gmail.com>
2019-07-06 17:56:55 -0300

    with threading

--- build_dataset.py
+++ build_dataset.py
@@ -71,10 +71,11 @@ def choose_cameras(cams, empty_bw, bw_threshold):
     return out
     #return out
 
-
-recinto1 = Recinto(2,4,i2c.openA1,i2c.closeA1,i2c.stop,i2c.go,lcd.LCD_LINE_1,1800000,my_cnn,size,i2c.openA2(),i2c.closeA2())
-recinto2 = Recinto(0,6,i2c.openB1,i2c.closeB1,i2c.stop,i2c.go,lcd.LCD_LINE_2,1500000,my_cnn,size,i2c.openB2(),i2c.closeB2())
-
+try:
+    recinto1 = Recinto(2,4,i2c.openA1,i2c.closeA1,i2c.stop,i2c.go,lcd.LCD_LINE_1,1800000,my_cnn,size,i2c.openA2(),i2c.closeA2())
+    recinto2 = Recinto(0,6,i2c.openB1,i2c.closeB1,i2c.stop,i2c.go,lcd.LCD_LINE_2,1500000,my_cnn,size,i2c.openB2(),i2c.closeB2())
+except:
+    exit(666)
 thread1 = threading.Thread(target = recinto1.take_photos())
 thread2 = threading.Thread(target = recinto2.take_photos())
 #flag  = False
