05f260e9e7ff44e15c068fd5227f99de0fe30e76
suttonnico <suttonnico2@gmail.com>
2019-09-21 16:42:05 -0300

    bash executable for gui

--- serie.py
+++ serie.py
@@ -1,6 +1,7 @@
 import shiftpi
+import RPi.GPIO as GPIO
 SER_PIN=17
 RCLK_PIN=27
 SRCLK_PIN=22
-shiftpi.pinsSetup({"ser": SER_PIN, "rclk": RCLK_PIN, "srclk": SRCLK_PIN})
+GPIO.pinsSetup({"ser": SER_PIN, "rclk": RCLK_PIN, "srclk": SRCLK_PIN})
 shiftpi.digitalWrite(1, shiftpi.HIGH)
