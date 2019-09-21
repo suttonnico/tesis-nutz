27ac23e133fe0f2f76c2a42b2462677004015fa8
suttonnico <suttonnico2@gmail.com>
2019-09-21 16:34:58 -0300

    bash executable for gui

--- serie.py
+++ serie.py
@@ -2,6 +2,5 @@ import shiftpi
 SER_PIN=17
 RCLK_PIN=27
 SRCLK_PIN=22
-shiftpi.pinsSetup({"ser": SER_PIN, "rclk": RCLK_PIN, "srclk": SRCLK_PIN}
-
+shiftpi.pinsSetup({"ser": SER_PIN, "rclk": RCLK_PIN, "srclk": SRCLK_PIN})
 shiftpi.digitalWrite(1, shiftpi.HIGH)
